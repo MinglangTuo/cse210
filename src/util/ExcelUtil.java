ca0cfefe29574b6f393cd7a0b5f31678fb37b9ec
Minglang.Tuo <1398212703@qq.com>
2019-04-14 15:19:41 +0800

    the excelutil has created

--- src/util/ExcelUtil.java
+++ src/util/ExcelUtil.java
@@ -6,113 +6,95 @@
 
 package util;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import org.apache.poi.hssf.usermodel.HSSFCell;
-import org.apache.poi.hssf.usermodel.HSSFRow;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.hssf.util.CellReference;
-import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
-import org.apache.poi.ss.usermodel.*;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import tweet.User;
 
-/**
- *
- * @author Tyler.Tuo
- */
-public class ExcelUtil {
+public class ExcelUtil{
     
+    public static List<String> TweetIdColumn = new ArrayList<>();
     
+    public static List<String> DateColumn = new ArrayList<>();
     
+    public static List<String> HourColumn = new ArrayList<>();
     
-    public static void main(String[] args) throws FileNotFoundException, IOException{
-      
-        
-        
-        DataFormatter formatter = new DataFormatter();
-        XSSFWorkbook wb = new XSSFWorkbook(new FileInputStream("excel.xlsx"));
-        XSSFSheet sheet = wb.getSheetAt(0);
-      /*  HSSFRow row = sheet.getRow(0);
-        if(row.getCell(0).getCellType() == HSSFCell.CELL_TYPE_STRING){
-            System.out.println(row.getCell(0).getStringCellValue());
+    public static List<String> UserNameColumn = new ArrayList<>();
+    
+    public static List<String> NicknameColumn = new ArrayList<>();
+    
+    public static List<String> TweetContentColumn = new ArrayList<>();
+    
+    public static List<String> FavsColumn = new ArrayList<>();
+    
+    public static List<String> RTsColumn = new ArrayList<>();
+    
+    public static List<String> LatitudeColumn = new ArrayList<>();
+    
+    public static List<String> LongitudeColumn = new ArrayList<>();
+    
+    public static List<String> FollowersColumn = new ArrayList<>();
+    
+    public static ArrayList<User> dealDataset(String filePath){
+        readDataSet(filePath);
+        ArrayList<User> users = new ArrayList();
+        int length = UserNameColumn.size();
+        for(int i = 0;i<length;++i){
+            String Tweet_Id = TweetIdColumn.get(i);
+            String Date =  DateColumn.get(i);
+            String Hour = HourColumn.get(i);
+            String User_name = UserNameColumn.get(i);
+            String Nick_name = NicknameColumn.get(i);
+            String Tweet_content = TweetContentColumn.get(i);
+            String Favs = FavsColumn.get(i);
+            String RTs = RTsColumn.get(i);
+            String Latitude = LatitudeColumn.get(i);
+            String Longitude = LongitudeColumn.get(i);
+            String Followers = FollowersColumn.get(i);
+            
+            users.add(new User(Tweet_Id,Date,Hour,User_name,Nick_name,Tweet_content,Favs,RTs,Latitude,Longitude,Followers));
+            
+           
         }
-        if(row.getCell(1).getCellType()==HSSFCell.CELL_TYPE_NUMERIC){
-            System.out.println(row.getCell(1).getDateCellValue());
-        }*/
-        for(Row row:sheet){
-            for(Cell cell:row){
-                CellReference cellRef = new CellReference(row.getRowNum(),cell.getColumnIndex());
-                String cellLocation = cellRef.formatAsString();
-                System.out.print(cellLocation);  // the location of cells
-                String column[] = cellLocation.split("")
-                
-                System.out.print(" - ");
-                
-                
-                //get the text that appears in the cell by getting the cell value and applying  any data format..
-                String text =  formatter.formatCellValue(cell);   
-                System.out.println(text);             //get the content of cell
-                
-               
- 
-                }
+         return users;
+    }
+        
+        @SuppressWarnings("resource")
+        public static void readDataSet(String filePath){
+            InputStream input;
+            try{
+                input = new FileInputStream(filePath);
+                XSSFWorkbook book = new XSSFWorkbook(input);
+                XSSFSheet sheet = book.getSheetAt(0);
+                XSSFRow row;
+                for(int i = sheet.getFirstRowNum()+1;i<sheet.getPhysicalNumberOfRows();++i){
+                    row = sheet.getRow(i);
+                    TweetIdColumn.add(row.getCell(0).getStringCellValue());
+                     DateColumn.add(row.getCell(1).getStringCellValue());
+                    HourColumn.add(row.getCell(2).getStringCellValue());
+                    UserNameColumn.add(row.getCell(3).getStringCellValue()); 
+                    NicknameColumn.add(row.getCell(4).getStringCellValue()); 
+                    TweetContentColumn.add(row.getCell(5).getStringCellValue()); 
+                    FavsColumn.add(row.getCell(6).getStringCellValue());
+                    RTsColumn.add(row.getCell(7).getStringCellValue()); 
+                    LatitudeColumn.add(row.getCell(8).getStringCellValue()); 
+                    LongitudeColumn.add(row.getCell(9).getStringCellValue()); 
+                    FollowersColumn.add(row.getCell(10).getStringCellValue()); }
+           }catch (FileNotFoundException IOE){
+                IOE.printStackTrace();
+           }catch (IOException IOE){
+                IOE.printStackTrace();
             }
-    }
-    
-      public void ChoiceAttribute(String attribute){  // use regular expression to extract the letter
-          Link TSlink = new link        
-          
-        switch(attribute){
-            case "A":
-                Tweet_Id = attribute;
         }
-    }
-    
+                
+   
     
     
     
 }
-        
-        
-        
-
-
-
-
-
-           /*    
-            //Alternatively, get the value and format it yourself
-                switch(cell.getCellType()){
-                    case STRING:
-                    {
-                        System.out.println(cell.getRichStringCellValue().getString());  }
-                        break;
-                    case NUMERIC:
-                        
-                        if(DateUtil.isCellDateFormatted(cell)){
-                           {
-                            System.out.println(cell.getDateCellValue());}
-                        }else{
-                            System.out.println(cell.getNumericCellValue());
-                        }
-                        break;
-                    case BOOLEAN:
-                        System.out.println(cell.getBooleanCellValue());
-                        break;
-                    case FORMULA:
-                        System.out.println(cell.getCellFormula());
-                        break;
-                    case BLANK:
-                        System.out.println();
-                        break;
-                    default:
-                        System.out.println();
-                        }
-                        
-                            */
-    
-
