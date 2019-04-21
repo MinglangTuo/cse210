fc1ec24895c0314fe12d6e6552a586732f847ceb
MinglangTuo <32598813+MinglangTuo@users.noreply.github.com>
2019-04-21 23:27:12 +0800

    Update Tweet.java

--- src/tweet/Tweet.java
+++ src/tweet/Tweet.java
@@ -6,41 +6,66 @@
 
 package tweet;
 
-import java.util.ArrayList;
+
 import java.util.HashMap;
 import tweet.User;
 import java.util.LinkedList;
 import java.util.Map;
 
 /**
- *
+ *this class represents the content of tweet
  * @author Tyler.Tuo
+ * @version V1.0
  */
 public class Tweet {
-private LinkedList<User> users;
-public Map<String,User> map = new HashMap<String,User>();  //judge the same name
+    /**
+	 * representing designing a tweet(users) by using linkedlist for users.
+	 */
+private LinkedList<User> tweet;
+    
+    /**
+	 * representing one User correspond one number of followers.
+	 */
+public Map<String,User> map = new HashMap<String,User>();  //judge the same name.
 
-
+    
+    /**
+	 * construct method without parameters.
+	 */
 public Tweet(){
     users = new LinkedList<User>();
 }
 
-
-public Tweet(LinkedList<User> users){
+/**
+	 * construct method with parameters
+	 * @param users a linkedList that stores tweet(users).
+	 */
+public Tweet(LinkedList<User> tweet){
         users = this.users;
 }
 
-
+    /**
+	 * get the list of User.
+	 * @return the linked list.
+	 */
 public LinkedList<User> getAllTweet(){
     return this.users;
 }
 
+    /**
+	 * add User item to the list.
+	 * @param user an User to be appended to the linked list of tweet(users).
+	 */
 public void addTweet(User user){
     users.add(user);
 }
-
-    /* the method is merge the same users*/
-    public ArrayList<User>mergeUsers(ArrayList<User> users){
+     
+     /**
+	 * the method is merge the same users in the tweet based on followers.
+	 * @param users(tweet) all of the users in the tweet.
+     * @return return the new linkedList of users(without the same)
+	 */
+    public LinkedList<User>mergeUsers(LinkedList<User> tweet){
         
         
         for(User i: users){
